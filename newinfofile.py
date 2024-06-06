8f3d1cede0be30d8c2612fae73df6f372bfd59ca
ilgazkuscu <40474649+ilgazkuscu@users.noreply.github.com>
2024-06-06 12:08:40 +0200

    Add files via upload

---
+++ newinfofile.py
@@ -0,0 +1,58 @@
+
+infos = {
+ 
+    'user.garciarm.39227472._000001.output_ntup.root': {
+                      'sumw'    :3.2e+09,
+                      'xsec'    :730},
+    'user.garciarm.39227472._000002.output_ntup.root': {
+                      'sumw'    :3.15e+09,
+                      'xsec'    :730},
+    'user.garciarm.39227472._000003.output_ntup.root': {
+                      'sumw'    :3.15e+09,
+                      'xsec'    :730},
+    'user.garciarm.39227472._000004.output_ntup.root': {
+                      'sumw'    :3.18e+09,
+                      'xsec'    :730},
+    'user.garciarm.39227472._000005.output_ntup.root': {
+                      'sumw'    :1.32e+09,
+                      'xsec'    :730},
+    'user.garciarm.39227472._000008.output_ntup.root': {
+                      'sumw'    :3.1e+09,
+                      'xsec'    :730},
+    'user.garciarm.39227472._000009.output_ntup.root': {
+                      'sumw'    :3.15e+09,
+                      'xsec'    :730},
+    'user.garciarm.39227472._000010.output_ntup.root': {
+                      'sumw'    :2.27e+09,
+                      'xsec'    :730},
+    'user.garciarm.39227472._000011.output_ntup.root': {
+                      'sumw'    :2.76e+09,
+                      'xsec'    :730},
+    'user.garciarm.39227472._000012.output_ntup.root': {
+                      'sumw'    :3.97e+09,
+                      'xsec'    :730},
+    'user.garciarm.39227472._000013.output_ntup.root': {
+                      'sumw'    :3.88e+09,
+                      'xsec'    :730},
+    'user.garciarm.39227472._000014.output_ntup.root': {
+                      'sumw'    :5.55e+08,
+                      'xsec'    :730},
+    'user.garciarm.39227472._000016.output_ntup.root': {
+                      'sumw'    :2.18e+09,
+                      'xsec'    :730},
+    'user.garciarm.39227472._000017.output_ntup.root': {
+                      'sumw'    :1.55e+09,
+                      'xsec'    :730},
+    'user.garciarm.39227472._000018.output_ntup.root': {
+                      'sumw'    :2.16e+09,
+                      'xsec'    :730},
+    'user.garciarm.39227472._000019.output_ntup.root': {
+                      'sumw'    :2.13e+09,
+                      'xsec'    :730},
+    'user.garciarm.39227472._000020.output_ntup.root': {
+                      'sumw'    :3.5e+08,
+                      'xsec'    :730},
+    
+
+    
+}
